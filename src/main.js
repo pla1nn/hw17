a8759285fea6c6ed1e381be985cc90c8a4914262
pla1n <danilkuisis@gmail.com>
2024-12-17 17:31:27 +0200

    sd

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const API_KEY = '47606801-75c62912983a7ab29411db2a8';
+const API_URL = 'https://pixabay.com/api/';
+
+let page = 0;
+
+const imageContainer = document.getElementById('image_container');
+const loadBtn = document.getElementById('load_more');
+
+const loadMore = async () => {
+    const response = await fetch(`${API_URL}?key=${API_KEY}&editors_choice=true&per_page=7&page=${page}`);
+    const data = await response.json();
+    data.hits.forEach(hit => {
+        imageContainer.innerHTML += `<div class="image_box"><img src="${image.imageURL}"></div>`
+    });
+}
+
+loadBtn.addEventListener('click', loadMore);
+loadMore();
